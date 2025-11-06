651ad53f9b8c6b2d8aca13285e163d25af4f3bef
PaulineBr0d <briaud.pauline@gmail.com>
2025-11-06 15:28:42 +0100

    first commit

---
+++ src/main/java/fr/m2i/cda/ecfspring/EcfspringApplication.java
@@ -0,0 +1,13 @@
+package fr.m2i.cda.ecfspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcfspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcfspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/m2i/cda/ecfspring/EcfspringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.m2i.cda.ecfspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcfspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
